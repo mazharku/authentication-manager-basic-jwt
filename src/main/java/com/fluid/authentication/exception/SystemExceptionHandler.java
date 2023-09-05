c98eb613dd95cff0777251bed2ba2ecd89b27e20
mazharibnazahur <mazhar.zahur@dohatec.com.bd>
2023-09-06 00:32:52 +0600

    update to spring boot 3.x.x

--- src/main/java/com/fluid/authentication/exception/SystemExceptionHandler.java
+++ src/main/java/com/fluid/authentication/exception/SystemExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.fluid.authentication.Exception;
+package com.fluid.authentication.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
